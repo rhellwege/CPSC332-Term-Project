b92471e5788e95a94cc44987a8139be318e3312a
Ryan Hellwege <rhellwege6@gmail.com>
2024-04-21 16:40:56 -0700

    fix testing environment

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,6 @@
-FROM php:7.4-apache
+FROM php:8.2-apache
 COPY homepage/ /var/www/html/
+# Enable Apache modules
+RUN a2enmod rewrite
+# Install any extensions you need
+RUN docker-php-ext-install mysqli pdo pdo_mysql
--- homepage/index.php
+++ homepage/index.php
@@ -1,8 +1,26 @@
+<?php 
+	echo "hello, this is php";
+	$hostName = 'mariadb';
+	$userName = 'username';
+	$password = 'password';
+	$dbName = 'database';
+
+	$link = mysqli_connect($hostName, $userName, $password, $dbName) or die("Unable to connect to host $hostName");
+	$SQL = "SELECT FirstName, LastName, DOB, Gender
+	FROM Patients WHERE Gender = '$Gender' ORDER BY
+	FirstName DESC";
+	$Patients = $link->query($SQL);
+	 $SQL = "INSERT INTO Patients (FirstName, LastName)
+	VALUES('$firstName', '$lastName')";	
+?>
 <html>
 <head>
 	<title>Hello World</title>
+	<script>
+
+	</script>
 </head>
 <body>
-<?php echo "hello, this is php";?>
+
 
 </body>
