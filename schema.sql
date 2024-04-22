37416a6a5a6ed1140fba1ee237eb14a559d89f9f
Ryan Hellwege <rhellwege6@gmail.com>
2024-04-22 12:16:13 -0700

    add ER model and sample sql DDL

---
+++ schema.sql
@@ -0,0 +1,13 @@
+USE university;
+
+CREATE TABLE Professor (
+	Ssn CHAR(15) NOT NULL,
+	FirstName VARCHAR(255) NOT NULL,
+	LastName VARCHAR(255) NOT NULL,
+
+	PRIMARY KEY (Ssn)
+);
+
+CREATE TABLE Department (
+
+);
