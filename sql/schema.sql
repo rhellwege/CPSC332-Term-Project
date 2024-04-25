aabe614c35bc647b9ffe9ab2ac994edb8deb6e21
Ryan Hellwege <rhellwege6@gmail.com>
2024-04-25 00:09:57 -0700

    add drops before creating table

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,5 +1,6 @@
 USE cpsc332e7;
 
+DROP TABLE IF EXISTS Professor;
 CREATE TABLE Professor (
 	Ssn       INT NOT NULL,
 	FirstName VARCHAR(255),
@@ -8,6 +9,7 @@ CREATE TABLE Professor (
 	PRIMARY KEY (Ssn)
 );
 
+DROP TABLE IF EXISTS Professor_Degrees;
 CREATE TABLE Professor_Degrees (
 	Degree VARCHAR(255) NOT NULL,
 	Professor_Ssn INT NOT NULL,
@@ -16,6 +18,7 @@ CREATE TABLE Professor_Degrees (
 	PRIMARY KEY (Degree, Professor_Ssn)
 );
 
+DROP TABLE IF EXISTS Department;
 CREATE TABLE Department (
 	DepartmentNumber INT NOT NULL,
 	DepartmentName VARCHAR(255),
@@ -27,6 +30,7 @@ CREATE TABLE Department (
 	PRIMARY KEY (DepartmentNumber)
 );
 
+DROP TABLE IF EXISTS Course;
 CREATE TABLE Course (
 	CourseNumber INT NOT NULL,
 	Textbook VARCHAR(255),
@@ -38,6 +42,7 @@ CREATE TABLE Course (
 	PRIMARY KEY (CourseNumber)
 );
 
+DROP TABLE IF EXISTS Course_Prerequisites;
 CREATE TABLE Course_Prerequisites (
 	CourseNumber INT NOT NULL,
 	Prerequisite_CourseNumber INT NOT NULL,
@@ -47,13 +52,14 @@ CREATE TABLE Course_Prerequisites (
 	FOREIGN KEY (Prerequisite_CourseNumber) REFERENCES Course(CourseNumber)
 );
 
+DROP TABLE IF EXISTS Course_Section;
 CREATE TABLE Course_Section (
 	SectionNumber INT NOT NULL,
 	Course_Number INT NOT NULL,
 	Classroom VARCHAR(255),
 	Seats INt,
-	BeginTime TIMESTAMP,
-	EndTime TIMESTAMP,
+	BeginTime TIME,
+	EndTime TIME,
 	Professor_Ssn INT,
 
 	PRIMARY KEY (SectionNumber, Course_Number),
@@ -61,6 +67,7 @@ CREATE TABLE Course_Section (
 	FOREIGN KEY (Course_Number) REFERENCES Course(CourseNumber)
 );
 
+DROP TABLE IF EXISTS Course_Section_Days;
 CREATE TABLE Course_Section_Days (
 	Section_Number INT NOT NULL,
 	Day VARCHAR(10),
@@ -69,6 +76,7 @@ CREATE TABLE Course_Section_Days (
 	PRIMARY KEY (Section_Number, Day)
 );
 
+DROP TABLE IF EXISTS Student;
 CREATE TABLE Student (
 	CWID INT NOT NULL,
 	Address VARCHAR(255),
@@ -81,6 +89,7 @@ CREATE TABLE Student (
 	FOREIGN KEY (Major_DepartmentNumber) REFERENCES Department(DepartmentNumber)
 );
 
+DROP TABLE IF EXISTS Student_Minor;
 CREATE TABLE Student_Minor (
 	CWID INT NOT NULL,
 	Minor_DepartmentNumber INT NOT NULL,
@@ -90,3 +99,13 @@ CREATE TABLE Student_Minor (
 	FOREIGN KEY (Minor_DepartmentNumber) REFERENCES Department(DepartmentNumber)
 );
 
+DROP TABLE IF EXISTS Student_Section_Enrollment;
+CREATE TABLE Student_Section_Enrollment (
+	CWID INT NOT NULL,
+	Course_Number INT NOT NULL,
+	Grade CHAR(2),
+
+	FOREIGN KEY (CWID) REFERENCES Student(CWID),
+	FOREIGN KEY (Course_Number) References Course(CourseNumber),
+	PRIMARY KEY(CWID, Course_Number)
+);
