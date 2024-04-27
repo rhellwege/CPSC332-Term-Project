19274c03fcd65b1304c3509a6148d93796162e3f
Ryan Hellwege <rhellwege6@gmail.com>
2024-04-27 00:44:26 -0700

    add formatting to schema, and add constraints to format grades and days of the week.

--- sql/schema.sql
+++ sql/schema.sql
@@ -2,7 +2,7 @@ USE cpsc332e7;
 
 DROP TABLE IF EXISTS Professor;
 CREATE TABLE Professor (
-	Ssn       INT NOT NULL,
+	Ssn       CHAR(9) NOT NULL,
 	FirstName VARCHAR(255),
 	LastName  VARCHAR(255),
 
@@ -11,8 +11,8 @@ CREATE TABLE Professor (
 
 DROP TABLE IF EXISTS Professor_Degrees;
 CREATE TABLE Professor_Degrees (
-	Degree VARCHAR(255) NOT NULL,
-	Professor_Ssn INT NOT NULL,
+	Degree        VARCHAR(255) NOT NULL,
+	Professor_Ssn CHAR(9)      NOT NULL,
 
 	FOREIGN KEY (Professor_Ssn) REFERENCES Professor(Ssn),
 	PRIMARY KEY (Degree, Professor_Ssn)
@@ -20,11 +20,11 @@ CREATE TABLE Professor_Degrees (
 
 DROP TABLE IF EXISTS Department;
 CREATE TABLE Department (
-	DepartmentNumber INT NOT NULL,
-	DepartmentName VARCHAR(255),
-	OfficeLocation VARCHAR(255),
-	DepartmentPhone VARCHAR(255),
-	Chairperson_Ssn INT,
+	DepartmentNumber INT NOT NULL AUTO_INCREMENT,
+	Chairperson_Ssn  CHAR(9),
+	DepartmentName   VARCHAR(255),
+	OfficeLocation   VARCHAR(255),
+	DepartmentPhone  VARCHAR(255),
 
 	FOREIGN KEY (Chairperson_Ssn) REFERENCES Professor(Ssn),
 	PRIMARY KEY (DepartmentNumber)
@@ -32,10 +32,10 @@ CREATE TABLE Department (
 
 DROP TABLE IF EXISTS Course;
 CREATE TABLE Course (
-	CourseNumber INT NOT NULL,
-	Textbook VARCHAR(255),
-	Units INT,
-	Title VARCHAR(255),
+	CourseNumber               INT NOT NULL AUTO_INCREMENT,
+	Textbook                   VARCHAR(255),
+	Units                      INT,
+	Title                      VARCHAR(255),
 	OfferedBy_DepartmentNumber INT,
 
 	FOREIGN KEY (OfferedBy_DepartmentNumber) REFERENCES Department(DepartmentNumber),
@@ -44,68 +44,70 @@ CREATE TABLE Course (
 
 DROP TABLE IF EXISTS Course_Prerequisites;
 CREATE TABLE Course_Prerequisites (
-	CourseNumber INT NOT NULL,
+	CourseNumber              INT NOT NULL,
 	Prerequisite_CourseNumber INT NOT NULL,
 
-	PRIMARY KEY (CourseNumber, Prerequisite_CourseNumber),
-	FOREIGN KEY (CourseNumber) REFERENCES Course(CourseNumber),
-	FOREIGN KEY (Prerequisite_CourseNumber) REFERENCES Course(CourseNumber)
+	FOREIGN KEY (CourseNumber)              REFERENCES Course(CourseNumber),
+	FOREIGN KEY (Prerequisite_CourseNumber) REFERENCES Course(CourseNumber),
+	PRIMARY KEY (CourseNumber, Prerequisite_CourseNumber)
 );
 
 DROP TABLE IF EXISTS Course_Section;
 CREATE TABLE Course_Section (
-	SectionNumber INT NOT NULL,
-	Course_Number INT NOT NULL,
-	Classroom VARCHAR(255),
-	Seats INt,
-	BeginTime TIME,
-	EndTime TIME,
-	Professor_Ssn INT,
+	SectionNumber INT NOT NULL AUTO_INCREMENT,
+	CourseNumber  INT NOT NULL,
+	Professor_Ssn CHAR(9),
+	Classroom     VARCHAR(255),
+	Seats         INT,
+	BeginTime     TIME,
+	EndTime       TIME,
 
-	PRIMARY KEY (SectionNumber, Course_Number),
 	FOREIGN KEY (Professor_Ssn) REFERENCES Professor(Ssn),
-	FOREIGN KEY (Course_Number) REFERENCES Course(CourseNumber)
+	FOREIGN KEY (CourseNumber)  REFERENCES Course(CourseNumber),
+	PRIMARY KEY (SectionNumber, CourseNumber)
 );
 
 DROP TABLE IF EXISTS Course_Section_Days;
 CREATE TABLE Course_Section_Days (
-	Section_Number INT NOT NULL,
-	Day VARCHAR(10),
+	SectionNumber INT NOT NULL,
+	Day           VARCHAR(2),
 
-	FOREIGN KEY (Section_Number) REFERENCES Course_Section(SectionNumber),
-	PRIMARY KEY (Section_Number, Day)
+	CONSTRAINT  CHK_DayFormat = CHECK Day IN ("M", "T", "W", "Th", "F", "Sa", "Su"),
+	FOREIGN KEY (SectionNumber) REFERENCES Course_Section(SectionNumber),
+	PRIMARY KEY (SectionNumber, Day)
 );
 
 DROP TABLE IF EXISTS Student;
 CREATE TABLE Student (
-	CWID INT NOT NULL,
-	Address VARCHAR(255),
-	FirstName VARCHAR(255),
-	LastName VARCHAR(255),
-	Phone CHAR(7),
+	CWID                   INT NOT NULL AUTO_INCREMENT,
 	Major_DepartmentNumber INT NOT NULL,
+	Address                VARCHAR(255),
+	FirstName              VARCHAR(255),
+	LastName               VARCHAR(255),
+	StudentPhone           CHAR(7),
 
-	PRIMARY KEY (CWID),
-	FOREIGN KEY (Major_DepartmentNumber) REFERENCES Department(DepartmentNumber)
+	FOREIGN KEY (Major_DepartmentNumber) REFERENCES Department(DepartmentNumber),
+	PRIMARY KEY (CWID)
 );
 
 DROP TABLE IF EXISTS Student_Minor;
 CREATE TABLE Student_Minor (
-	CWID INT NOT NULL,
+	CWID                   INT NOT NULL,
 	Minor_DepartmentNumber INT NOT NULL,
 
-	PRIMARY KEY (CWID, Minor_DepartmentNumber),
-	FOREIGN KEY (CWID) REFERENCES Student(CWID),
-	FOREIGN KEY (Minor_DepartmentNumber) REFERENCES Department(DepartmentNumber)
+	FOREIGN KEY (CWID)                   REFERENCES Student(CWID),
+	FOREIGN KEY (Minor_DepartmentNumber) REFERENCES Department(DepartmentNumber),
+	PRIMARY KEY (CWID, Minor_DepartmentNumber)
 );
 
 DROP TABLE IF EXISTS Student_Section_Enrollment;
 CREATE TABLE Student_Section_Enrollment (
-	CWID INT NOT NULL,
-	Course_Number INT NOT NULL,
-	Grade CHAR(2),
+	CWID         INT NOT NULL,
+	CourseNumber INT NOT NULL,
+	Grade        VARCHAR(2),
 
-	FOREIGN KEY (CWID) REFERENCES Student(CWID),
-	FOREIGN KEY (Course_Number) References Course(CourseNumber),
-	PRIMARY KEY(CWID, Course_Number)
+	CONSTRAINT  CHK_GradeFormat = CHECK Grade IN ("A", "A-", "A+", "B", "B-", "B+", "C", "C-", "C+", "D", "D-", "D+", "F")
+	FOREIGN KEY (CWID)            REFERENCES Student(CWID),
+	FOREIGN KEY (CourseNumber)    REFERENCES Course(CourseNumber),
+	PRIMARY KEY(CWID, CourseNumber)
 );
