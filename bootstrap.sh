72b2e6f03397cb7104f88cc5fd4a563a9da85972
Ryan Hellwege <rhellwege6@gmail.com>
2024-04-28 20:17:26 -0700

    .

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-#
+
+github_username="rhellwege"
+github_repo="CPSC332-Term-Project"
+mysql_username="cs332e7"
+mysql_password="2wJ3Nbfa"
+
 download() {
     # wipe directory
     echo ''
@@ -8,7 +13,7 @@ download() {
     find . ! -name 'bootstrap.sh' -exec rm -rf {} +
     # download the tarball
     echo "Downloading project archive..."
-    wget --no-check-certificate https://github.com/rhellwege/CPSC332-Term-Project/archive/master.tar.gz
+    wget --no-check-certificate "https://github.com/$github_username/$github_repo/archive/master.tar.gz"
 
     # extracting the tarball
     echo ''
@@ -21,10 +26,8 @@ download() {
     echo "cleaning up..."
     echo ''
     rm ./master.tar.gz
-    mv ./CPSC332-Term-Project-main/* .
-    rm -rf ./CPSC332-Term-Project-main/
-
-
+    mv "./$github_repo/*" .
+    rm -rf ./$github_repo/
 }
 
 # download from scratch 
@@ -44,7 +47,7 @@ read -p "Run sql DROP commands? (Only needed if run before.) (y/n): " yn
     case $yn in
         [Yy]* ) # Dropping tables
 echo "Dropping tables...";
-mysql -h mariadb -u cs332e7 -p2wJ3Nbfa < ./sql/drop.sql;
+mysql -h mariadb -u $mysql_username -p$mysql_password < ./sql/drop.sql;
 break;;
         [Nn]* ) break;;
         * ) echo "Please answer yes or no.";;
@@ -57,7 +60,7 @@ read -p "Run sql CREATE commands? (Only needed if all tables are dropped or firs
     case $yn in
         [Yy]* ) # Dropping tables
 echo "Generating tables...";
-mysql -h mariadb -u cs332e7 -p2wJ3Nbfa < ./sql/schema.sql;
+mysql -h mariadb -u $mysql_username -p$mysql_password < ./sql/schema.sql;
 break;;
         [Nn]* ) break;;
         * ) echo "Please answer yes or no.";;
@@ -70,7 +73,7 @@ read -p "Run sql INSERT commands? (Only needed if all tables are dropped or firs
     case $yn in
         [Yy]* ) # Dropping tables
 echo "Inserting data...";
-mysql -h mariadb -u cs332e7 -p2wJ3Nbfa < ./sql/insert.sql;
+mysql -h mariadb -u $mysql_username -p$mysql_password < ./sql/insert.sql;
 break;;
         [Nn]* ) break;;
         * ) echo "Please answer yes or no.";;
