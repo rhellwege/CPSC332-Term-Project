876eebd0cb921afee62baf1ed3392db865a8cb3d
Ryan Hellwege <rhellwege6@gmail.com>
2024-05-11 18:20:29 -0700

    add drop to startup script

--- startup/startup.sh
+++ startup/startup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo '-------- DROPPING DATABASE TABLES... --------'
+mysql -u USERNAME -pPASSWORD < /sql/drop.sql
 echo '-------- CREATING DATABASE TABLES... --------'
 mysql -u USERNAME -pPASSWORD < /sql/schema.sql
 echo '-------- INSERTING DATABASE TABLES... --------'
