3137353def235a7f8bda2c89997d0385559997a4
Ryan Hellwege <rhellwege6@gmail.com>
2024-05-11 18:32:02 -0700

    I hate sql

--- sql/schema.sql
+++ sql/schema.sql
@@ -68,7 +68,7 @@ CREATE TABLE Course_Section (
 
 	FOREIGN KEY (Professor_Ssn) REFERENCES Professor(Ssn),
 	FOREIGN KEY (CourseNumber)  REFERENCES Course(CourseNumber),
-	PRIMARY KEY (CourseNumber, SectionNumber),
+	PRIMARY KEY (CourseNumber, SectionNumber)
 );
 
 CREATE TABLE Course_Section_Days (
