d89bd1315bdc85e3f6760e32fec911be6b106318
Ryan Hellwege <rhellwege6@gmail.com>
2024-05-11 18:23:30 -0700

    update drop sql

--- sql/drop.sql
+++ sql/drop.sql
@@ -1,13 +1,13 @@
 -- WARNING: Deletes all tables from the database!
 USE USERNAME;
 
-DROP TABLE Student_Minor;
-DROP TABLE Student_Section_Enrollment;
-DROP TABLE Professor_Degrees;
-DROP TABLE Course_Section_Days;
-DROP TABLE Course_Prerequisites;
-DROP TABLE Course_Section;
-DROP TABLE Course;
-DROP TABLE Student;
-DROP TABLE Department;
-DROP TABLE Professor;
+DROP TABLE IF EXISTS Student_Minor;
+DROP TABLE IF EXISTS Student_Section_Enrollment;
+DROP TABLE IF EXISTS Professor_Degrees;
+DROP TABLE IF EXISTS Course_Section_Days;
+DROP TABLE IF EXISTS Course_Prerequisites;
+DROP TABLE IF EXISTS Course_Section;
+DROP TABLE IF EXISTS Course;
+DROP TABLE IF EXISTS Student;
+DROP TABLE IF EXISTS Department;
+DROP TABLE IF EXISTS Professor;
